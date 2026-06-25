80a4d4bc1ee4bc353d2c70ddad14815b8b5c78e7
ljh143 <1690553298@qq.com>
2026-06-25 10:45:39 +0800

    第二版

--- WarehouseManagement/obj/Debug/net8.0/WarehouseManagement.AssemblyInfo.cs
+++ WarehouseManagement/obj/Debug/net8.0/WarehouseManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarehouseManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9a7b9d02d91a197bb295786cfa5be9d0f9f61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20280183e6aff7dae94eae631185a2901fd05df8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarehouseManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarehouseManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
